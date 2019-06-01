e907fdaa5e70b7f7a573d7c09cf35865ff156fcd
Jahn <ioannis.kiwe@gmail.com>
2019-06-01 22:03:03 +0200

    Init

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export HOST_IP=`hostname -I | cut -d' ' -f 1`
+
+echo "Setting stack IP to $HOST_IP"
+
+docker stack deploy -c docker-stack-arm64v8.yml kafka-zookeeper-schema-registry
