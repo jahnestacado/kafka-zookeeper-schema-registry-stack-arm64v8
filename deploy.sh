75c7e889805e1fa0f57db762e546f9a70e9c1482
jahnestacado <ioannis.kiwe@gmail.com>
2020-01-28 13:55:20 +0000

    Updated volume paths and constrains

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export HOST_IP=`hostname -I | cut -d' ' -f 1`
 
 echo "Setting stack IP to $HOST_IP"
 
-docker stack deploy -c docker-stack-arm64v8.yml kafka-zookeeper-schema-registry
+docker stack deploy -c docker-stack-arm64v8.yml kafka
